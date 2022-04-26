e60a59f5d348668b19e2a2caa5f4831a0a1d7256
OlegNevmitiy <godnebeles@gmail.com>
2022-04-26 23:06:58 +0300

    hmm

--- 2course/coursework/Restaurant/Restaurant/Storage.cs
+++ 2course/coursework/Restaurant/Restaurant/Storage.cs
@@ -8,6 +8,47 @@ namespace Restaurant
 {
     public class Storage
     {
+        public Dictionary<Ingredient, int> Ingredients { get; private set; }
+
+        public Storage()
+        {
+            Ingredients = new Dictionary<Ingredient, int>();
+        }
+
+        public Storage(Dictionary<Ingredient, int> ingredients)
+        {
+            Ingredients = ingredients;
+        }
+
+        public int GetCountDishCanCook(List<Ingredient> ingredientsList)
+        {
+            int totalCount = 0;
+
+            foreach(var neenedIngredient in ingredientsList)
+            {
+                foreach(var ingredientKey in Ingredients.Keys)
+                {
+                    if(neenedIngredient.Name == ingredientKey.Name)
+                    {
+                        int count = Ingredients[ingredientKey];
+
+                        if(count == 0)
+                            return 0;
+                        else
+                        {
+                            if(totalCount == 0)
+                                totalCount = count;
+                            else
+                                totalCount = count < totalCount ? count : totalCount;
+                        }
+                    }
+                }
+                if (totalCount == 0)
+                    return 0;
+            }
+
+            return totalCount;
+        }
 
     }
 }
