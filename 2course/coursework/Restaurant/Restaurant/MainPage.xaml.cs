3654f83995e2eca304f73813d83b36f58484d033
OlegNevmitiy <godnebeles@gmail.com>
2022-05-06 13:51:47 +0300

    вф

--- 2course/coursework/Restaurant/Restaurant/MainPage.xaml.cs
+++ 2course/coursework/Restaurant/Restaurant/MainPage.xaml.cs
@@ -24,5 +24,10 @@ namespace Restaurant
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- 2course/coursework/Restaurant/Restaurant/MainWindow.xaml.cs
+++ 2course/coursework/Restaurant/Restaurant/MainWindow.xaml.cs
@@ -20,38 +20,57 @@ namespace Restaurant
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainPage mainPage = new MainPage();
+        ListOfOrdersPage listOfOrdersPage = new ListOfOrdersPage();
+        BudgetCalculatorPage budgetCalculatorPage = new BudgetCalculatorPage();
+        class Dishes
+        {
+            public int id { get; set; }
+            public string dishName { get; set; }
+            public int price { get; set; }
+            public int count { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = mainPage;
 
             Dish dish = new Dish("Soup", 13.0, new Weight(500, UnitOfWeight.G));
 
             DateTimeContainer d1 = new DateTimeContainer(5, 2, 2022);
             DateTimeContainer d2 = new DateTimeContainer(5, 2, 2022);
             DateTimeContainer d3 = new DateTimeContainer(6, 2, 2022);
-            Dictionary<DateTimeContainer, int> dictionary = new Dictionary<DateTimeContainer, int>();
-            dictionary.Add(d1, 2);
 
-           
+            Dictionary<DateTimeContainer, int> dictionary = new Dictionary<DateTimeContainer, int>();
+            dictionary.Add(d1, 2);        
 
             bool result = d1.Equals(d2);
             bool result1 = d1.Equals(d3);
-        }
 
-        
+            mainPage.DataGridDishes.ItemsSource = new List<Dishes>() { new Dishes() { id = 6, dishName = "Ketchup", price = 204320, count = 3 } };
+            
+        }
+      
 
         private void ToMenuButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new MainPage();
+            ChangePage((Button)sender, mainPage);
         }
+
         private void ToListOfOrders_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new ListOfOrdersPage();
+            ChangePage((Button)sender, listOfOrdersPage);
         }
 
         private void ToCalculatorButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Content = new BudgetCalculatorPage();
+            ChangePage((Button)sender, budgetCalculatorPage);
+        }
+
+        public void ChangePage(Button btn, Page page)
+        {
+            MainFrame.Content = page;
         }
     }
 }
