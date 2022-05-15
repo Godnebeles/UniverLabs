67777849be205e242f3421c321284c9c432bf73c
OlegNevmitiy <godnebeles@gmail.com>
2022-05-15 22:41:36 +0300

    sync

--- 2course/coursework/Restaurant/Restaurant/obj/Debug/net5.0-windows/Pages/DishEditingPage.g.i.cs
+++ 2course/coursework/Restaurant/Restaurant/obj/Debug/net5.0-windows/Pages/DishEditingPage.g.i.cs
@@ -61,7 +61,7 @@ namespace Restaurant {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Restaurant;component/pages/disheditingpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Restaurant;V1.0.0.0;component/pages/disheditingpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\DishEditingPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
