69f16b9047614ed1f7715386c6a72f51b1e5e42b
OlegNevmitiy <godnebeles@gmail.com>
2022-06-01 23:40:14 +0300

    base class ViewModel.cs

--- 2course2semestr/coursework/sale_of_vehicles/sale_of_vehicles/Views/Windows/MainWindow.xaml.cs
+++ 2course2semestr/coursework/sale_of_vehicles/sale_of_vehicles/Views/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace sale_of_vehicles
+namespace sale_of_vehicles.Views.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
